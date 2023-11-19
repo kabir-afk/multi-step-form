6c7d875071c454528b6d7fdfb7b70d48a7ba4fe6
kabir-afk <kabirsingh12b@gmail.com>
2023-11-19 23:53:41 +0530

    Update Info component
    - Add validation to name input
    - Add error message for invalid name
    - Add validation to email input
    - Add validation to phone number input
    - Add flexbox to name input and error message
    - Make error message red (generated by blackbox.ai )

--- src/components/Step-1/Info.js
+++ src/components/Step-1/Info.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Info.css";
 import NextBtn from "../NextBtn/NextBtn";
 import { Link } from "react-router-dom";
 
 export default function Info() {
+  const [nameError, setNameError] = useState(false);
+  const [nameErrorMsg, setNameErrorMsg] = useState("Invalid character in name");
+  function ValidateName(e) {
+    const validNameRegex = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    if (e.target.value === "") {
+      setNameErrorMsg("This field is required");
+    } else {
+      validNameRegex.test(e.target.value)
+        ? setNameError(false)
+        : setNameError(true);
+    }
+  }
+  function ErrorMsg() {
+    return <div className="errorMsg">{nameErrorMsg}</div>;
+  }
+  function validateEmail() {
+    console.log("hellow");
+  }
+  function validatePhoneNumber() {
+    console.log("hellow");
+  }
   return (
     <form>
       <h1>Personal info</h1>
       <p>Please provide your name, email address, and phone number.</p>
 
-      <label htmlFor="name">Name</label>
+      <div className="flex">
+        <label htmlFor="name">Name</label> {nameError && <ErrorMsg />}
+      </div>
       <input
         type="text"
         name="name"
         id="name"
         placeholder="e.g. Stephen King"
         autoComplete="given-name"
+        onInput={ValidateName}
       />
 
       <label htmlFor="email">Email Address</label>
@@ -25,6 +49,7 @@ export default function Info() {
         id="email"
         placeholder="e.g. stephenking@lorem.com"
         autoComplete="on"
+        onInput={validateEmail}
       />
 
       <label htmlFor="phoneNumber">Phone Number</label>
@@ -33,6 +58,7 @@ export default function Info() {
         name="phoneNumber"
         id="phoneNumber"
         placeholder="e.g. +1 234 567 890"
+        onInput={validatePhoneNumber}
       />
       <Link to="/Plan">
         <NextBtn />
