576b951fd480a400533ef19446b4b4ff46dbbb48
kabir-afk <kabirsingh12b@gmail.com>
2023-11-21 14:58:19 +0530

    Added custom checkbox in AddOn component

--- src/components/Step-3/AddOn.js
+++ src/components/Step-3/AddOn.js
@@ -4,53 +4,38 @@ import PrevBtn from "../PrevBtn/PrevBtn";
 import { Link } from "react-router-dom";
 import "./Addon.css";
 export default function AddOn() {
-  function hello() {
-    console.log("hellow");
-  }
   return (
     <form>
       <h1>Pick add-ons</h1>
       <p>Add-ons help enhance your gaming experience.</p>
 
       <div className="addon">
-        <label className="checkbox">
-          <input type="checkbox" checked onChange={hello} />
-          <span className="custom-checkbox">
-            <img src="/assets/images/icon-checkmark.svg" alt="icon-check" />
-          </span>
-        </label>
-        <article htmlFor="service">
+        {/* <img src="/assets/images/icon-checkmark.svg" alt="icon-check" /> */}
+        <input type="checkbox" id="service" />
+        <label htmlFor="service">
           <div className="plan-heading">Online service</div>
           <p>Access to multiplayer games</p>
-        </article>
+        </label>
         <div className="monthly">+$1/mo</div>
       </div>
 
       <div className="addon">
-        <label className="checkbox">
-          <input type="checkbox" checked onChange={hello} />
-          <span className="custom-checkbox">
-            <img src="/assets/images/icon-checkmark.svg" alt="icon-check" />
-          </span>
-        </label>
-        <article htmlFor="storage">
+        {/* <img src="/assets/images/icon-checkmark.svg" alt="icon-check" /> */}
+        <input type="checkbox" id="storage" />
+        <label htmlFor="storage">
           <div className="plan-heading">Larger storage</div>
           <p>Extra 1TB of cloud save</p>
-        </article>
+        </label>
         <div className="monthly">+$2/mo</div>
       </div>
 
       <div className="addon">
-        <label className="checkbox">
-          <input type="checkbox" checked onChange={hello} />
-          <span className="custom-checkbox">
-            <img src="/assets/images/icon-checkmark.svg" alt="icon-check" />
-          </span>
-        </label>
-        <article htmlFor="profile">
+        {/* <img src="/assets/images/icon-checkmark.svg" alt="icon-check" /> */}
+        <input type="checkbox" id="profile" />
+        <label htmlFor="profile">
           <div className="plan-heading">Customizable Profile</div>
           <p>Custom theme on your profile</p>
-        </article>
+        </label>
         <div className="monthly">+$2/mo</div>
       </div>
 
